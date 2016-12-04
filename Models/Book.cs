399da082ae8ba31b8e616538852e9ca15a307491
bsokat <barissokat@gmail.com>
2016-12-04 12:21:04 +0300

    Model classes were defined

---
+++ Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace SearchingSortingPaging.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        [DisplayName("Kitap Adı")]
+        public string Name { get; set; }
+        public long ISBN { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        [DisplayName("Publication Date")]
+        public int PublicationDate { get; set; }
+        public string Price { get; set; }
+        [DisplayName("Reduced Price")]
+        public string ReducedPrice { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace SearchingSortingPaging.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
