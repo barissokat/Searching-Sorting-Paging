ba68ca3a850518c8006da7c39eab1c9e2e291cbc
bsokat <barissokat@gmail.com>
2016-12-05 09:01:38 +0300

    Book searching were implemente in book list view

---
+++ Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using SearchingSortingPaging.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SearchingSortingPaging.Controllers
+{
+    public class BookController : Controller
+    {
+        private BookContext db = new BookContext();
+        // GET: Book
+        public ActionResult Index(string option, string searchString)
+        {
+            if (option == "Name")
+            {
+                return View(db.Books.Where(b => b.Name.Contains(searchString) || searchString == null).ToList());
+            }
+            else if (option == "Author")
+            {
+                return View(db.Books.Where(b => b.Author.Contains(searchString) || searchString == null).ToList());
+            }
+            else if (option == "Publisher")
+            {
+                return View(db.Books.Where(b => b.Publisher.Contains(searchString) || searchString == null).ToList());
+            }
+            else
+            {
+                return View(db.Books.Where(b => b.Name.StartsWith(searchString) || searchString == null).ToList());
+            }
+        }
+    }
+}
