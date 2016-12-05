6c81cee2622471968e96980cd7c3a2123d7945d4
bsokat <barissokat@gmail.com>
2016-12-05 08:23:54 +0300

    Category controller were defined

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using SearchingSortingPaging.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SearchingSortingPaging.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private BookContext db = new BookContext();
+        // GET: Category
+        public ActionResult Index(string searchString)
+        {
+            var query = db.Categories.AsQueryable();
+            query = query.Where(c => c.Name.Contains(searchString) || searchString == null);
+            return View(query.ToList());
+        }
+    }
+}
